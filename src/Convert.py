89f1da8b43fdc4533739db5ab7870b35234862a9
alechuang98 <alechuang98@gmail.com>
2020-08-10 22:54:09 +0800

    add readme

--- src/Convert.py
+++ src/Convert.py
@@ -147,10 +147,10 @@ def main():
                 else:
                     res_arcs.append([center.tolist(), out_road[0], in_road[0]])
 
-    plot_log(args.output, res_nodes, res_edges, res_arcs)
+    # plot_log(args.log, res_nodes, res_edges, res_arcs)
     res_nodes, res_edges = add_point(res_nodes, res_edges, copy.deepcopy(res_arcs))
-    log2json(args.log, res_nodes, res_edges)
-    plot_log(args.output, res_nodes, res_edges, res_arcs)
+    log2json(args.output, res_nodes, res_edges)
+    # plot_log(args.log, res_nodes, res_edges, {})
 
 if __name__ == "__main__":
     main()
